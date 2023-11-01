1a7d7131144b3796e274b5930f10797beaf2a960
Pedro Negrao <macedopedro117@yahoo.com.br>
2023-10-31 22:30:03 -0300

    Mudanças no arquvio index.js

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const btnSubmit = document.querySelector(".btn-submit");
+const btnAll = document.querySelectorAll(".btn");
+
+btnAll.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    const botaoSelecionado = document.querySelector(".btn.selecionado");
+    if (botaoSelecionado) {
+      botaoSelecionado.classList.remove("selecionado");
+    }
+
+    botao.classList.add("selecionado");
+  });
+});
+
+function btnClicado(event) {
+  let booleano = event;
+
+  if (
+    booleano === "1" ||
+    booleano === "2" ||
+    booleano === "3" ||
+    booleano === "4" ||
+    booleano === "5"
+  ) {
+    if (
+      btnSubmit.addEventListener("click", () => {
+        Swal.fire({
+          titleText: `Thank you!
+          You select ${booleano} out of 5`,
+          icon: "success",
+          imageWidth: 200,
+          imageHeight: 100,
+          text: "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!",
+          imageAlt: "Custom image",
+        });
+      })
+    );
+  }
+}
